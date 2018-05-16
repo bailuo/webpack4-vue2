8e7d25f50c6ebc833d4ca35ebea7dc1e5e9fbe5c
dxvgef <dxvgef@outlook.com>
2018-05-16 11:55:44 +0800

    update

--- src/main.vue
+++ src/main.vue
@@ -8,47 +8,8 @@
 </template>
 
 <style>
+    /* 导入一个全局作用域的样式文件，会影响整个项目，在其它组件里无需重复导入此文件 */
     @import './global/common.scss';
-</style>
-<style scoped>
-    /* 滑动过程中的样式 */
-    .left-enter-active, .left-leave-active, .right-enter-active, .right-leave-active {
-        will-change: transform;
-        transition: all .5s;
-    }
-    /* 从左边滑入前的位置 */
-    .left-enter {
-        transform: translateX(-100%);
-    }
-    /* 从左边滑入后的位置 */
-    .left-enter-to {
-        transform: translateX(0);
-    }
-    /* 从左边滑入的组件在离开前的位置 */
-    .left-leave {
-        transform: translateX(0);
-    }
-    /* 从左边滑入的组件在离开后的位置 */
-    .left-leave-to {
-        transform: translateX(-100%);
-    }
-
-    /* 从右边滑入前的位置 */
-    .right-enter {
-        transform: translateX(100%);
-    }
-    /* 从右边滑入后的位置 */
-    .right-enter-to {
-        transform: translateX(0);
-    }
-    /* 从右边滑入的组件在离开前的位置 */
-    .right-leave {
-        transform: translateX(0);
-    }
-    /* 从右边滑入的组件在离开后的位置 */
-    .right-leave-to {
-        transform: translateX(100%);
-    }
 </style>
 
 <script>
@@ -59,8 +20,9 @@
                 transitionName: ''
             };
         },
-        //监控路由切换
+        //监视状态
         watch: {
+            //路由
             $route(to, from) {
                 //更改页面标题
                 if (to.meta.title !== undefined && to.meta.title !== null && to.meta.title !== '') {
@@ -68,10 +30,10 @@
                 }
                 //如果to索引大于from索引,判断为前进,否则为后退
                 if (to.meta.index > from.meta.index) {
-                    //从右边进入屏幕
+                    //新组件从右边进入屏幕，旧组件从左边离开屏幕
                     this.transitionName = 'right';
                 } else {
-                    //从左边进入屏幕
+                    //新组件从左边进入屏幕，旧组件从右边离开屏幕
                     this.transitionName = 'left';
                 }
             }
